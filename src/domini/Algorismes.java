f6271300e9f16b8c7a3f89dfffef0bee062b037f
xavi <antiwindows17@gmail.com>
2015-11-01 17:50:48 +0100

    Afegida classe Algorismes

---
+++ src/domini/Algorismes.java
@@ -0,0 +1,18 @@
+package domini;
+
+//import java.util.*;
+
+//////////////////////////////
+//
+// Algorismes
+//
+// Conjunt d'algorismes per a la implementació
+// del joc Hidato
+//
+//////////////////////////////
+
+public class Algorismes {
+    private int tauler[][];
+    private int solucio[][];
+
+}
