2590f96ec7c56a88475823424ed3fbb2c71aa3d9
xavi <antiwindows17@gmail.com>
2015-11-01 18:02:36 +0100

    Afegida classe Algorismes

--- src/domini/Algorismes.java
+++ src/domini/Algorismes.java
@@ -9,6 +9,11 @@ package domini;
 // Conjunt d'algorismes per a la implementació
 // del joc Hidato
 //
+// Podem suposar per al tractament d'un tauler:
+// -1 equival a casella sense utilitzar (bloquejada)
+// 0 equival a casella buida (per a omplir amb un numero)
+// [1..n] valor que te la casella corresponent en un tauler N*N
+//
 //////////////////////////////
 
 public class Algorismes {
