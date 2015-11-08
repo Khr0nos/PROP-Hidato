45819bdff9f0978f8d435192282cb150352907f1
Llistoman <josepll.plamarcell@gmail.com>
2015-11-08 16:32:40 +0100

    Afegit el package excepcions, on aniran totes les excepcions que fem servir, separades per grups

---
+++ src/excepcions/sudoku/ExcepcionPosicionFueraRango.java
@@ -0,0 +1,10 @@
+package excepcions.sudoku;
+
+public class ExcepcionPosicionFueraRango extends Exception {
+	public static final long serialVersionUID = 44L;
+	
+	@Override
+	public String getMessage() {
+		return "La posicion esta fuera del rango permitido";
+	}
+}
