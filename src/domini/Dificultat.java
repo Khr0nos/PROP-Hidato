a5b4ee7677ef6c7c3d7556412eb678ec1e885435
xavi <antiwindows17@gmail.com>
2015-11-14 17:46:37 +0100

    actualització dificultat + imports

--- src/domini/Dificultat.java
+++ src/domini/Dificultat.java
@@ -1,7 +1,7 @@
 package domini;
 
 abstract public class Dificultat {
-	private static final String[] d = {"3x3", "4x4", "5x5", "6x6", "7x7", "8x8", "9x9"};
+	private static final String[] d = {"facil","medio","dificil"};
 	
 	public static boolean esValida(String s) {
 		for (int i=0; i<d.length; ++i) {
