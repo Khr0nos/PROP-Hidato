2065101469aa5edec6352076df58e01d628d2c98
Xavi <antiwindows17@gmail.com>
2015-12-01 12:48:56 +0100

    Interficie inicial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import domini.Vistes.CtrlVistes;
+
+public class Main {
+  private static CtrlVistes CV;
+  public static void main(String[] args) {
+    CV = new CtrlVistes();
+    CV.run();
+  }
+}
---
+++ src/domini/Vistes/CtrlVistes.java
@@ -0,0 +1,31 @@
+package domini.Vistes;
+
+import domini.Algorismes.Algorismes;
+import domini.FabricaHidato.FabricaHidato;
+import domini.Partida.CtrlPartida;
+import domini.Ranking.CTRLRanking;
+import domini.TaulerHidato.CtrlTauler;
+import domini.Usuari.CtrlUser;
+
+public class CtrlVistes {
+  private static CtrlPartida CP;
+  private static CtrlTauler CT;
+  private static CTRLRanking CR;
+  private static CtrlUser CU;
+  private static Algorismes A;
+  private static FabricaHidato F;
+
+  public CtrlVistes() {
+    CP = new CtrlPartida();
+    CT = new CtrlTauler();
+    CR = new CTRLRanking();
+    CU = new CtrlUser();
+    A = new Algorismes();
+    F = new FabricaHidato();
+
+  }
+
+  public void run() {
+    //cridat al Main
+  }
+}
