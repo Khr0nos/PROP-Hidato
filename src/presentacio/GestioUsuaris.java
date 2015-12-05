bc0c07e7d69a3a9e793f6ac8f2f0c5b2a6d07b11
Xavi <antiwindows17@gmail.com>
2015-12-05 19:26:08 +0100

    Gestio usuaris

--- src/presentacio/GestioUsuaris.java
+++ src/presentacio/GestioUsuaris.java
@@ -1,8 +1,12 @@
 package presentacio;
 
+import domini.CtrlDomini;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.util.Vector;
 
 /**
  * Created by Xavi on 05/12/2015.
@@ -18,6 +22,12 @@ public class GestioUsuaris extends JFrame {
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     setVisible(true);
     setLocationRelativeTo(null);
+    Vector<Vector<String>> valors = CtrlDomini.getDadesUsuaris();
+    Vector<String> cols = new Vector<String>(2);
+    cols.add("Username");
+    cols.add("Password");
+    Usuaris = new JTable(valors, cols);
+    //Falta veure com fer que la taula mostri la info adequadament
     Back.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
@@ -26,5 +36,6 @@ public class GestioUsuaris extends JFrame {
       }
     });
   }
-  //Modificar la Jtable per a mostrar i poder editar usuaris
+
+  //Modificar la Jtable per a mostrar i poder editar usuaris a través del CtrlDomini
 }
