907e322412e1da3621061b0becb74fb60a0e36cf
xavi <antiwindows17@gmail.com>
2015-12-07 18:04:12 +0100

    Threading

--- src/presentacio/Main.java
+++ src/presentacio/Main.java
@@ -1,10 +1,17 @@
 package presentacio;
 
+import javax.swing.*;
+
 /**
  * Created by MAX on 01/12/2015.
  */
 public class Main {
     public static void main(String arg[]){
-        Entra e = new Entra();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Entra();
+            }
+        });
     }
 }
